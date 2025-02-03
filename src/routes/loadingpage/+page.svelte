98013f57a425fc55899017140eda7da74a798984
Sai-adi <masterful.adi@gmail.com>
2025-02-03 16:27:25 +0530

    loading page for laptop screen view

--- src/routes/loadingpage/+page.svelte
+++ src/routes/loadingpage/+page.svelte
@@ -84,7 +84,7 @@
       bind:this={videoElement}
       on:loadedmetadata={setVideoStartTime}
     >
-      <source src="/show11.mp4" type="video/mp4" />
+      <source src="/show12.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
   </div>
